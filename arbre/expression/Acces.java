1c2883fea1655d5903356c11842fed11f2dcdc37
AnaisDecolle <anais.decolle8@etu.univ-lorraine.fr>
2016-03-03 15:13:01 +0100

    acces

---
+++ arbre/expression/Acces.java
@@ -0,0 +1,40 @@
+package plic.arbre.expression;
+
+import plic.tds.TDS;
+import plic.tds.entrees.EntreeVar;
+
+public class Acces extends Expression {
+
+	private String idf;
+	
+	public Acces(String i) {
+		idf = i;
+	}
+	
+	public boolean estEntier() {
+		return true;
+	}
+	
+	@Override
+	public boolean verify() {
+		// TODO Auto-generated method stub
+		return TDS.getInstance().identifier(new EntreeVar(idf)) != null;
+	}
+
+	@Override
+	public String toMips() {
+		StringBuilder s = new StringBuilder();
+		if(verify()) {
+			s.append("# Acces variable " + idf + "\n");
+			int decalage = TDS.getInstance().identifier(new EntreeVar(idf)).getDep();
+			s.append("lw $v0, " + decalage + "($s7)\n");
+		}
+		return s.toString();
+	}
+
+	@Override
+	public String toString() {
+		return "Acces [idf=" + idf + "]";
+	}
+
+}
